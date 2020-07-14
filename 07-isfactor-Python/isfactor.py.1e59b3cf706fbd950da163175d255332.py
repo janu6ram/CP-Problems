e041cbfcf1c94c46e37dd9e63f0080a16cbb2985
janu6ram <janu6ram@msitprogram.net>
2020-07-14 10:56:35 +0530

    distance

---
+++ 07-isfactor-Python/isfactor.py.1e59b3cf706fbd950da163175d255332.py
@@ -0,0 +1,11 @@
+# Write the function isFactor(f, n) that takes
+# two int values f and n, and returns True
+# if f is a factor of n, and False otherwise.
+# Note that every integer is a factor of 0.
+
+
+
+def fun_isfactor(f, n):
+	# replace with your solution
+	if n % f == 0:
+		return
