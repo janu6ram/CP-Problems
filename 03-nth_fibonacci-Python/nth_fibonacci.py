b12d80a0b35fa36110cb2bf26f38a845f2b6d1f0
janu6ram <janu6ram@msitprogram.net>
2020-07-15 22:14:58 +0530

    nth fibnocci

--- 03-nth_fibonacci-Python/nth_fibonacci.py
+++ 03-nth_fibonacci-Python/nth_fibonacci.py
@@ -1,12 +1,13 @@
-# Background: The Fibonacci numbers are defined by F(n) = F(n-1) + F(n-2). 
-# There are different conventions on whether 0 is a Fibonacci number, 
-# and whether counting starts at n=0 or at n=1. Here, we will assume that 
-# 0 is not a Fibonacci number, and that counting starts at n=0, 
-# so F(0)=F(1)=1, and F(2)=2. With this in mind, write the function 
+# Background: The Fibonacci numbers are defined by F(n) = F(n-1) + F(n-2).
+# There are different conventions on whether 0 is a Fibonacci number,
+# and whether counting starts at n=0 or at n=1. Here, we will assume that
+# 0 is not a Fibonacci number, and that counting starts at n=0,
+# so F(0)=F(1)=1, and F(2)=2. With this in mind, write the function
 # nthfibonaccinumber(n) that takes a non-negative int n and returns the nth Fibonacci number.
 
 
-
 def fun_nthfibonaccinumber(n):
-	# your code goes Here
-	return 0
+    # your code goes Here
+    if n <= 1:
+        return 1
+    return fun_nthfibonaccinumber(n-2) + fun_nthfibonaccinumber(n-1)
