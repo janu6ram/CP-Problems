8fa321cf59085aa58dffd4606e49f7675639338d
janu6ram <janu6ram@msitprogram.net>
2020-07-16 11:13:13 +0530

    Jul 16  11:13:12

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -1,5 +1,5 @@
-# Write the function nthHappyNumber(n) which takes a non-negative integer 
-# and returns the nth happy number (where the 0th happy number is 1). 
+# Write the function nthHappyNumber(n) which takes a non-negative integer
+# and returns the nth happy number (where the 0th happy number is 1).
 # Here are some test assertions for you:
 # assert(nthHappyNumber(0) == 1)
 # assert(nthHappyNumber(1) == 7)
@@ -10,6 +10,8 @@
 # assert(nthHappyNumber(6) == 28)
 # assert(nthHappyNumber(7) == 31)
 
+from ishappynumber import ishappynumber
+
 
 def fun_nth_happy_number(n):
-	return 0
+    return 0
