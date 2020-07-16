b797b4d4d29c3297c761a3ba0f1cfe4c51c57285
janu6ram <janu6ram@msitprogram.net>
2020-07-16 13:50:46 +0530

    nth happy number

---
+++ 01-nth_happy_number-Python/ishappynumber.py
@@ -0,0 +1,32 @@
+# ishappynumber(n) [10 pts]
+# A happy number is a number defined by the following process: Starting with any positive integer, replace the
+# number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will
+# stay ), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1
+# are happy numbers.
+
+# Write the function ishappynumber(n) which takes a possibly-negative integer and returns True if it is happy and
+# False otherwise. Note that all numbers less than 1 are not happy. Here are some test assertions for you:
+# assert(ishappynumber(-7) == False)
+# assert(ishappynumber(1) == True)
+# assert(ishappynumber(2) == False)
+# assert(ishappynumber(97) == True)
+# assert(ishappynumber(98) == False)
+# assert(ishappynumber(404) == True)
+# assert(ishappynumber(405) == False)
+
+def ishappynumber(n):
+    # your code goes here
+    if n == 1:
+        return True
+    if n == 4 or n <= 0:
+        return False
+    sum = 0
+    while n != 0:
+        rem = n % 10
+        sum = sum + rem**2
+        n = n//10
+    # print(sum)
+    return ishappynumber(sum)
+
+
+print(ishappynumber(2))
