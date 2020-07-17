10a129a43457a5e8905a9d36dd41ab4df75cfcde
janu6ram <janu6ram@msitprogram.net>
2020-07-17 16:30:48 +0530

    Jul 17  16:30:47

--- 08-isrotated-Python/test_isrotated.py
+++ 08-isrotated-Python/test_isrotated.py
@@ -1,19 +1,20 @@
 
-import os,sys
-sys.path.append(os.getcwd())
-from isrotated import isrotated
 import pytest
+from isrotated import isrotated
+import os
+import sys
+sys.path.append(os.getcwd())
 
 
-@pytest.mark.parametrize("a, b, result",[
-	("XYZ", "ZXY", True),
-	("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "BCDEFGHIJKLMNOPQRSTUVWXYZA", True),
-	("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "JKLMNOPQRSTUVWXYZABCDEFGHI", True),
-	("12345", "54321", True),
-	("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ACDEFGHIJKLMNOPQRSTUVWXYZB", False),
-	("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "BCDEFGHIJKLMNOPQRSTUVWXYZZ", False),
-	("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ABCDEFGHIJKLMNPQRSTUVWXYZ", False),
-	("ABCDEFGHIJKLMNOPQRSUVWXYZ", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", False)
+@pytest.mark.parametrize("a, b, result", [
+    ("XYZ", "ZXY", True),
+    ("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "BCDEFGHIJKLMNOPQRSTUVWXYZA", True),
+    ("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "JKLMNOPQRSTUVWXYZABCDEFGHI", True),
+    ("12345", "54321", False),
+    ("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ACDEFGHIJKLMNOPQRSTUVWXYZB", False),
+    ("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "BCDEFGHIJKLMNOPQRSTUVWXYZZ", False),
+    ("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ABCDEFGHIJKLMNPQRSTUVWXYZ", False),
+    ("ABCDEFGHIJKLMNOPQRSUVWXYZ", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", False)
 ])
-def test_isrotated(a, b, result):    
+def test_isrotated(a, b, result):
     assert isrotated(a, b) == result
