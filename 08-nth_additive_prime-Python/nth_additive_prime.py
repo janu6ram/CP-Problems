28fa33ea972dccd3a80af54c41706c7302a78acc
janu6ram <janu6ram@msitprogram.net>
2020-07-19 01:37:05 +0530

    Jul 19  01:37:04

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -6,3 +6,35 @@
 
 def fun_nth_additive_prime(n):
     primes = []
+    i = 2
+    while len(primes) != n:
+        if prime_number(i) and additive(i):
+            primes.append(i)
+    return primes[n]
+
+
+def prime_number(n):
+    if n <= 1:
+        return False
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i < n:
+        if n % i == 0 or n % (i+2) == 0:
+            return False
+        i = i + 6
+    return True
+
+
+def additive(n):
+    sum = 0
+    while n != 0:
+        rem = n % 10
+        n = n//10
+        sum += rem
+    if prime_number(sum):
+        return True
+    else:
+        return False
