a10922f00722f9cfe6a7c36d4a2b7ee6ffb420ef
janu6ram <janu6ram@msitprogram.net>
2020-07-19 04:33:13 +0530

    Jul 19  04:33:12

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -2,17 +2,24 @@
 # frequent character in the string. If there are multiple characters that
 # can account as K-th the most frequent character then, print any one of them.
 
+import collections
+
 
 def fun_kth_occurrences(s, n):
-    dict1 = {}
-    for i in s:
-        if i == " ":
-            dict1 = {}
-            continue
-        if i in dict1:
-            dict1[i] += 1
-            if dict1[i] == n+1:
-                return i
-        else:
-            dict1[i] = 1
-    return s.strip()[0]
+    # dict1 = {}
+    # for i in s:
+    #     if i == " ":
+    #         dict1 = {}
+    #         continue
+    #     if i in dict1:
+    #         dict1[i] += 1
+    #         if dict1[i] == n+1:
+    #             return i
+    #     else:
+    #         dict1[i] = 1
+    # return s.strip()[0]
+    frequent = [item for item in collections.Counter(s).most_common()]
+    print(frequent)
+
+
+fun_kth_occurrences("hsdibdouvbeiovbfeibvdsioubidsbvviefadbcildfab", 4)
