6a9a7daa3c761407fc707eb31b18cba97c5225e1
janu6ram <janu6ram@msitprogram.net>
2020-07-19 00:18:12 +0530

    Jul 19  00:18:11

--- 05-binarytreepractice-Python/test_solution.py
+++ 05-binarytreepractice-Python/test_solution.py
@@ -1,27 +1,36 @@
-import os,sys
-sys.path.append(os.getcwd())
-from binarytree import BinaryTree, Node
 import pytest
+from binarytree import BinaryTree, Node
+import os
+import sys
+sys.path.append(os.getcwd())
 
 tree = BinaryTree(1)
-@pytest.mark.parametrize('check,result',[
+
+
+@pytest.mark.parametrize('check,result', [
     (tree.search(4), False), (tree.search(6), False)
 ])
 def test_classiness1(check, result):
+    print("test", tree.search(4))
     assert check == result
+
 
 tree.root.left = Node(2)
 tree.root.right = Node(3)
 tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
+tree.print_tree()
 
-@pytest.mark.parametrize('check,result',[
+
+@pytest.mark.parametrize('check,result', [
     (tree.search(4), True), (tree.search(6), False),
 ])
 def test_classiness2(check, result):
+    print("test", tree.search(4))
     assert check == result
 
-@pytest.mark.parametrize('check,result',[
+
+@pytest.mark.parametrize('check,result', [
     (tree.search(None), False), (tree.search("6"), False),
 ])
 def test_classiness2(check, result):
