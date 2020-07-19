55d53e4d9f615ea4334c8516ab653dff4c884a5a
janu6ram <janu6ram@msitprogram.net>
2020-07-19 13:16:42 +0530

    short run python

--- 09-shortenlongruns-Python/shortenlongruns.py
+++ 09-shortenlongruns-Python/shortenlongruns.py
@@ -1,14 +1,29 @@
 # shortenLongRuns(L, k) [15 pts]
-# Write the non-destructive function shortenLongRuns(L, k) that takes a list L and a positive integer k, and 
-# non-destructively returns a similar list, only without any run of k consecutive equal values in L. This means that 
-# any values that would otherwise produce a consecutive run of k elements are not present. 
-# For example: 
-# assert(shortenLongRuns([2, 3, 5, 5, 5, 3], 2) == [2, 3, 5, 3]) 
-# assert(shortenLongRuns([2, 3, 5, 5, 5, 3], 3) == [2, 3, 5, 5, 3]) 
-# Note: your function may not just create a copy of L and call the destructive version of this function (below) on 
+# Write the non-destructive function shortenLongRuns(L, k) that takes a list L and a positive integer k, and
+# non-destructively returns a similar list, only without any run of k consecutive equal values in L. This means that
+# any values that would otherwise produce a consecutive run of k elements are not present.
+# For example:
+# assert(shortenLongRuns([2, 3, 5, 5, 5, 3], 2) == [2, 3, 5, 3])
+# assert(shortenLongRuns([2, 3, 5, 5, 5, 3], 3) == [2, 3, 5, 5, 3])
+# Note: your function may not just create a copy of L and call the destructive version of this function (below) on
 # that copy and return it. Instead, you must directly construct the result here.
 
 
 def shortenlongruns(L, k):
-	# Your code goes here
-	pass
+    # Your code goes here
+    count = 1
+    j = 0
+    for i in L[:-1]:
+        j += 1
+        if i == L[j]:
+            count += 1
+            if count == k:
+                L.remove(i)
+                j -= 1
+                count -= 1
+            continue
+        count = 1
+    return L
+
+
+print(shortenlongruns([2, 3, 5, 5, 5, 3], 3))
