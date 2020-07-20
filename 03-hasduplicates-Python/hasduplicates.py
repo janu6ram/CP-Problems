1013524d45de7afbfc587887413037e9f3ace0ce
janu6ram <janu6ram@msitprogram.net>
2020-07-20 10:43:23 +0530

    has duplicates

--- 03-hasduplicates-Python/hasduplicates.py
+++ 03-hasduplicates-Python/hasduplicates.py
@@ -1,8 +1,15 @@
 # hasDuplicates(L) [15 pts]
-# Write the function hasDuplicates(L) that takes a 2d list L of arbitrary values, and returns True if L 
-# contains any duplicate values (that is, 
+# Write the function hasDuplicates(L) that takes a 2d list L of arbitrary values, and returns True if L
+# contains any duplicate values (that is,
 # if any two values in L are equal to each other), and False otherwise.
 
 def hasduplicates(L):
-	# Your code goes here
-	pass
+    # Your code goes here
+    dict1 = {}
+    for i in L:
+        for j in i:
+            if j not in dict1:
+                dict1[j] = 1
+            else:
+                return True
+    return False
