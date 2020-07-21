52e55e2054544d5d41d7a58926fa4325eef8b9fd
janu6ram <janu6ram@msitprogram.net>
2020-07-21 19:19:52 +0530

    Jul 21  19:19:51

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -1,12 +1,30 @@
-# Without using iteration and without using strings, 
-# write the recursive function onlyEvenDigits(L), 
-# that takes a list L of non-negative integers 
-# (you may assume that), and returns a new list of 
-# the same numbers only without their odd digits 
-# (if that leaves no digits, then replace the number with 0). 
-# So: onlyEvenDigits([43, 23265, 17, 58344]) returns [4, 226, 0, 844]. 
-# Also the function returns the empty list if the original list is empty. 
+# Without using iteration and without using strings,
+# write the recursive function onlyEvenDigits(L),
+# that takes a list L of non-negative integers
+# (you may assume that), and returns a new list of
+# the same numbers only without their odd digits
+# (if that leaves no digits, then replace the number with 0).
+# So: onlyEvenDigits([43, 23265, 17, 58344]) returns [4, 226, 0, 844].
+# Also the function returns the empty list if the original list is empty.
 # Remember to not use strings. You may not use loops/iteration in this problem.
 
-def fun_recursion_onlyevendigits(l): 
-		return []
+def fun_recursion_onlyevendigits(l):
+    if len(l) == 0:
+        return []
+    num = l.pop(0)
+    even = only_even(num)
+    print(even)
+    return [num] + fun_recursion_onlyevendigits(l)
+
+
+def only_even(num):
+    if num == 0:
+        return 0
+    rem = num % 10
+    num //= 10
+    if rem % 2 != 0:
+        return only_even(num)
+    return only_even(num) * 10 + rem
+
+
+fun_recursion_onlyevendigits([43, 23265, 17, 58344])
