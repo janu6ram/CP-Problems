787097352dd8d4446c1863b89968cbeec1b468f8
janu6ram <janu6ram@msitprogram.net>
2020-07-25 19:35:16 +0530

    Jul 25  19:35:15

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -1,9 +1,25 @@
 # longestDigitRun(n) [20 pts]
-# Write the function longestDigitRun(n) that takes a possibly-negative int value n and returns 
-# the digit that has the longest consecutive 
+# Write the function longestDigitRun(n) that takes a possibly-negative int value n and returns
+# the digit that has the longest consecutive
 # run, or the smallest such digit if there is a tie. So, longestDigitRun(117773732) returns 7 (
-# because there is a run of 3 consecutive 7's), 
+# because there is a run of 3 consecutive 7's),
 # as does longestDigitRun(-677886).
+import sys
+
+
 def longestdigitrun(n):
-	# Your code goes here
-	pass
+    # Your code goes here
+    count = 1
+    max = 0
+    prev = sys.maxint
+    while n != 0:
+        rem1 = n % 10
+        n //= 10
+        rem2 = n % 10
+        if rem1 == rem2:
+            count += 1
+            if count > max and rem1 <= prev:
+                max = count
+                prev = rem1
+        else:
+            count = 1
