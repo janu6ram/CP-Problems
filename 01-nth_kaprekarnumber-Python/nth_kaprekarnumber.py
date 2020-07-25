e808b3883b4912fb1f6c4426a79e1b4739e438e7
janu6ram <janu6ram@msitprogram.net>
2020-07-25 21:17:28 +0530

    Jul 25  21:17:28

--- 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
+++ 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
@@ -1,13 +1,21 @@
-# Background: a Kaprekar number is a non-negative integer, the representation of whose square 
-# can be split into two possibly-different-length parts (where the right part is not zero) 
-# that add up to the original number again. For instance, 45 is a Kaprekar number, because 
-# 45**2 = 2025 and 20+25 = 45. You can read more about Kaprekar numbers here. The first several 
-# Kaprekar numbers are: 1, 9, 45, 55, 99, 297, 703, 999 , 2223, 2728,... 
-# With this in mind, write the function nthKaprekarNumber(n) that takes a non-negative int n 
+# Background: a Kaprekar number is a non-negative integer, the representation of whose square
+# can be split into two possibly-different-length parts (where the right part is not zero)
+# that add up to the original number again. For instance, 45 is a Kaprekar number, because
+# 45**2 = 2025 and 20+25 = 45. You can read more about Kaprekar numbers here. The first several
+# Kaprekar numbers are: 1, 9, 45, 55, 99, 297, 703, 999 , 2223, 2728,...
+# With this in mind, write the function nthKaprekarNumber(n) that takes a non-negative int n
 # and returns the nth Kaprekar number, where as usual we start counting at n==0.
 
 
 import math
 
+
 def fun_nth_kaprekarnumber(n):
-    return 1;
+    kaprekars = []
+    i = 1
+    while len(kaprekars) != n+1:
+        square = pow(i, 2)
+        if check_sum(square) == i:
+            kaprekars.append(i)
+        i += 1
+    return kaprekars[n]
