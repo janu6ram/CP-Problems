cff52507814658837b0bb7cc3dd267eea0d77a63
janu6ram <janu6ram@msitprogram.net>
2020-07-26 13:34:11 +0530

    Jul 26  13:34:11

--- 08-nth_circularprime-Python/nthcircularprime.py
+++ 08-nth_circularprime-Python/nthcircularprime.py
@@ -1,10 +1,18 @@
 # nthCircularPrime(n) [20 pts]
-# Write the function nthCircularPrime that takes a non-negative int n and returns the nth 
-# Circular prime, which is a prime number that does not contain any 0's and such that all the 
-# numbers resulting from rotating its digits are also prime. The first Circular primes are 2, 3, 
-# 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97, 113, 131, 197... To see why 197 is a Circular prime, 
+# Write the function nthCircularPrime that takes a non-negative int n and returns the nth
+# Circular prime, which is a prime number that does not contain any 0's and such that all the
+# numbers resulting from rotating its digits are also prime. The first Circular primes are 2, 3,
+# 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97, 113, 131, 197... To see why 197 is a Circular prime,
 # note that 197 is prime, as is 971 (rotated left), as is 719 (rotated left again).
 
+circular_primes = [2]
+
+
 def nthcircularprime(n):
-	# Your code goes here
-	pass
+    # Your code goes here
+    while len(circular_primes) != n+1:
+        i = 3
+        if circular_prime(i):
+            circular_primes.append(i)
+        i += 2
+    return circular_primes[n]
