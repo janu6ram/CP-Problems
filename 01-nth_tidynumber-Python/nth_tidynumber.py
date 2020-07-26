612ef209a7bb6999fa3bf444ce8a458afda43fda
janu6ram <janu6ram@msitprogram.net>
2020-07-26 17:10:23 +0530

    tidy number python

--- 01-nth_tidynumber-Python/nth_tidynumber.py
+++ 01-nth_tidynumber-Python/nth_tidynumber.py
@@ -5,6 +5,29 @@
 # fun_nth_tidynumber(5) = 6
 # fun_nth_tidynumber(15) = 17
 # fun_nth_tidynumber(35) = 46
+import sys
+
 
 def fun_nth_tidynumber(n):
-    tidy
+    tidy_numbers = []
+    i = 1
+    while len(tidy_numbers) != n+1:
+        if increasing(i):
+            tidy_numbers.append(i)
+        i += 1
+    return tidy_numbers[n]
+
+
+def increasing(n):
+    prev = sys.maxsize
+    while n != 0:
+        rem = n % 10
+        if prev >= rem:
+            prev = rem
+        else:
+            return False
+        n //= 10
+    return True
+
+
+print(fun_nth_tidynumber(35))
